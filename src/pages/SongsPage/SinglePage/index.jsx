7130db5de6997666ba33f0f268aab7c069112deb
joao-moutinho <jonasmoutinho5@hotmail.com>
2022-05-04 17:36:43 +0100

    Done Job

--- src/pages/SongsPage/SinglePage/index.jsx
+++ src/pages/SongsPage/SinglePage/index.jsx
@@ -6,29 +6,42 @@ import { useSelector } from "react-redux";
 export const SinglePage = () => {
   const songs = useSelector((state) => state.songs);
   const { id } = useParams();
+  const artists = useSelector((state) => state.artists)
+
+  console.log(artists)
 
   const data = Object.entries(songs).find(([keys, values]) => id === keys)[1];
 
-  /* 
-  name: "Flute",
-        artistId: 16,
-        genre: "rock",
-        year: 2001,
-        album: "Flute Release",
-        albumArt: "https://i.imgur.com/XqQXKZY.jpg",
+  const filteredArtist = Object.entries(artists)
 
-*/
+  console.log(filteredArtist)
+
+  const filterMyArtist = () => {
+    return filteredArtist.filter((artist) => artist[1].id === data.artistId)[0][1]
+  }
+
+  
+
   return (
     <div
       className="single-page"
-      style={{ backgroundImage: `url(${data.albumArt})` }}
+      style={ {  background: `linear-gradient(rgba(0,0,0,.7), rgba(0,0,0,.7)), url("${data.albumArt}")`}}
     >
       <div className="albumImage"></div>
       <div className="Info">
-        <div className="artistName"></div>
-        <div className="musicName"></div>
-        <div className="musicYear"> </div>
-        <div className="albumName"></div>
+      <div className="container" style={{  background: `url("${filterMyArtist().photo}")` , backgroundSize:"cover"}} >
+   
+   <div className="container__profile">
+    <img
+     src= {data.albumArt}
+     alt="people"
+    />
+    <div className="container__profile__text">
+     <h2>{data.name}</h2>
+     <p className="artist-name-by">by <b>{filterMyArtist().name}</b></p>
+    </div>
+   </div>
+  </div>
       </div>
     </div>
   );
--- src/pages/SongsPage/components/Song.jsx
+++ src/pages/SongsPage/components/Song.jsx
@@ -1,45 +1,49 @@
-import './style.scss';
-import React, { useState } from 'react';
+import "./style.scss";
+import React, { useState } from "react";
 import { BsPlayFill } from "react-icons/bs";
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { useSelector } from "react-redux";
+import { useNavigate,Link } from "react-router-dom";
 
+export const Song = ({ song, id }) => {
+  const navigate = useNavigate();
+  const [showPlay, setShowPlay] = useState(false);
+  const artists = useSelector((state) => state.artists);
 
-export const Song = ({song, id}) =>{
+  const findArtistName = (id) => {
+    return Object.values(artists).map((artist) => {
+      if (artist.id === id && artist.id != undefined) {
+        return artist.name;
+      }
+    });
+  };
 
-    const navigate = useNavigate();
-    const [showPlay, setShowPlay] = useState(false);    
-    const artists = useSelector((state) => state.artists);
-
-    const findArtistName = (id) => {
-        return Object.values(artists).map((artist) => {
-          if (artist.id === id && artist.id != undefined) {
-            return  artist.name;
-          }
-        });
-      };
-
-    return(
-        <div className="song" onMouseLeave={()=> setShowPlay(false)} onMouseOver={() => setShowPlay(true)}>
-        <div className="id">{showPlay ? <BsPlayFill size={25} /> : id}</div>
-        <div onClick={() => navigate(`/songs/${id}`)} className="img">
-          <img  src={song.albumArt} width="70px" height="70px" />
-        </div>
-        <div className="name-info">
-          <div className="name-artist">
-            {findArtistName(song.artistId)}
-          </div>
-          <div className="name-music">
-            <p>{song.name}</p>
-          </div>
-        </div>
-        <div className="album">
-          <p>{song.album}</p>
-        </div>
-        <div className="year">
-          <p>{song.year}</p>
-        </div>
-        
+  return (
+    <div
+      className="song"
+      onMouseLeave={() => setShowPlay(false)}
+      onMouseOver={() => setShowPlay(true)}
+    >
+      <div className="id">{showPlay ? <BsPlayFill size={25} /> : id}</div>
+      <div  className="img">
+        <img src={song.albumArt} width="70px" height="70px" />
       </div>
-    );
-}
+      <div className="name-info">
+        <div className="name-artist">
+          <Link  className="name-artist-link" to={`/artist/${song.artistId}`}>
+            {" "}
+            {findArtistName(song.artistId)}
+          </Link>
+        </div>
+        <div onClick={() => navigate(`/songs/${id}`)} className="name-music">
+          <p>{song.name}</p>
+        </div>
+      </div>
+      <div className="album">
+        <p>{song.album}</p>
+      </div>
+      <div className="year">
+        <p>{song.year}</p>
+      </div>
+    </div>
+  );
+};
