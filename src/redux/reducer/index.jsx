b6d15e1c9ea66b4afd53ed30ae2a063a4b61899c
Rafael Martins <41962783+rmzaoo@users.noreply.github.com>
2022-05-10 14:45:01 +0100

    finish

--- src/redux/reducer/index.jsx
+++ src/redux/reducer/index.jsx
@@ -2,6 +2,9 @@ import { initialState } from "./initialState";
 
 function youSongReducer(state = initialState, action) {
   switch (action.type) {    
+
+    case 'GET_ARTISTS_MUSIC':
+      return Object.entries(state.songs).filter(([key, song]) => song.artistId === action.artistId)
     default:
       return state;
   }
