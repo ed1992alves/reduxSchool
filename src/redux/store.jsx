5c8b0424c395d288bd676b38a6621e52ff0725e9
RuicTeixeira <ruiteixeirapc@gmail.com>
2022-05-11 16:45:05 +0100

    add

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,6 +1,8 @@
-import youSongReducer from './reducer'
-import { createStore } from 'redux'
+import {applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from "redux-thunk";
+import youSongReducer from './reducer';
 
-const store = createStore(youSongReducer)
+const store = createStore(youSongReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 export default store;
